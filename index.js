176fb3979bd1d56d62eb06221ef428f01e88a89a
ryanfalco13@gmail.com <ryanfalco13@gmail.com>
2023-05-31 14:40:02 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    const message = `This Saturday, I want to ${activity}!`;
+    console.log(message);
+    return message;
+}
+
+console.log(saturdayFun());
+console.log(saturdayFun('go hiking'));
+
+const activity1 = saturdayFun('roller-skate');
+console.log(activity1);
+
+const activity2 = saturdayFun('go hiking');
+console.log(activity2);
+
+const mondayWork = function(activity = 'go to the office') {
+    const message = `This Monday, I will ${activity}.`;
+    console.log(message);
+    return message;
+};
+
+console.log(mondayWork());
+
+function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("amazing"));
+  
+
+
